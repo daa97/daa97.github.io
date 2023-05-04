954234bf6cf6d692b4d0749f760ea371fecf3a5a
David Austin <114694180+daa97@users.noreply.github.com>
2023-05-04 14:39:00 -0400

    fixed CFE colors

--- assets/cfe/cfe_model.js
+++ assets/cfe/cfe_model.js
@@ -17,12 +17,12 @@ cfeParams.addEventListener("load", (ev) => {
   SetUp(6,1,.4,[0,.5,.5]); // turbine
   SetUp(7,0.9,.5,[0,.7,.32]); // URN
   SetUp(8,0.8,.68,[.02,.02,.02]); // rubber
-  SetUp(9,1,.55,[.08,.08,.07]); // flanges
-  SetUp(10,0.9,.5,[.3,0,0]); // PV
-  SetUp(11,0.9,.5,[0,.1,.2]); // adaptor
-  SetUp(12,0.9,.5,[.48,.05,.4]); // stators
-  SetUp(13,0.9,.5,[0,.15,.08]); // end cap
-  SetUp(14,0.9,.5,[.12,.12,.05]); // bearing cap
+  SetUp(14,1,.55,[.08,.08,.07]); // flanges
+  SetUp(9,0.9,.5,[.3,0,0]); // PV
+  SetUp(10,0.9,.5,[0,.08,.2]); // adaptor
+  SetUp(11,0.9,.5,[.48,.05,.4]); // stators
+  SetUp(12,0.9,.5,[0,.15,.08]); // end cap
+  SetUp(13,0.9,.5,[.12,.12,.05]); // bearing cap
   SetUp(15,1,.2,[.8,.6,.1]); // brass
   SetUp(16,0.9,.5,[.2,0,.2]); // inlet manifold
   SetUp(17,1,.33,[0.5,0.52,0.56]); // base plate
